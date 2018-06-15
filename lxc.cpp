8cc027a5a4c48805c062ecb47e7b2d15d203e35a
gate80 <39673562+gate80@users.noreply.github.com>
2018-06-15 13:36:20 +0500

    1.0

---
+++ lxc.cpp
@@ -0,0 +1,88 @@
+#include "lxc.h"
+#include <sstream>	// sstream
+#include <string>   // string
+#include <fstream>	// fille >> <<
+#include <list>		// list
+using namespace std;
+lxc::lxc()
+{
+}
+lxc::~lxc()
+{
+}
+
+void lxc::readFile(string fileName, string &str)
+{
+	ifstream in(fileName.c_str(), ios::binary);
+	ostringstream out;
+	str.clear();
+	out << in.rdbuf();
+	out.str().swap(str);
+}
+void lxc::split(string str, list<string> &list)
+{
+	istringstream ss(str);
+	string String;
+	while (ss >> String)
+		list.push_back(String);
+}
+void lxc::run(string command, int i) // to file
+{
+	command += " > console.linux.txt";
+	system(command.c_str());
+}
+void lxc::run(string command)
+{
+	system(command.c_str());
+}
+void lxc::installPackage()
+{
+	run("sudo apt-get install lxc");
+}
+void lxc::create_container(string container, string dist, string relese, string arch)
+{
+	run("sudo lxc-create -t download -n " + container + " -- --dist " + dist + " --release " + relese + " --arch " + arch);
+}
+void lxc::start_container(string container)
+{
+	run("sudo lxc-start " + container);
+}
+void lxc::ls_container()
+{
+	run("sudo lxc-ls", 1);
+}
+void lxc::ls_container_list(list <string> &ls)
+{
+	run("sudo lxc-ls", 1);
+	string s;
+	readFile("t.txt", s);
+	split(s, ls);
+}
+void lxc::info_container(string container)
+{
+	run("sudo lxc-info " + container, 1);
+}
+void lxc::stop_container(string container)
+{
+	run("sudo lxc-stop " + container);
+}
+void lxc::copy_container(string container, string copy_name)
+{
+	run("sudo lxc-copy " + container + " -N" + copy_name);
+}
+void lxc::del_container(string container)
+{
+	run("sudo lxc-destroy " + container);
+}
+void lxc::freeze_container(string container)
+{
+	run("sudo lxc-freeze " + container);
+}
+void lxc::unfreeze_container(string container)
+{
+	run("sudo lxc-unfreeze " + container);
+}
+void lxc::memory_limit(string container, int M)
+{
+	run("sudo lxc-cgroup -n " + container + " memory.limit_in_bytes " + to_string(M) + "M");
+}
---
+++ lxc.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <sstream>	// sstream
+#include <string>   // string
+#include <fstream>	// fille >> <<
+#include <list>		// list
+using namespace std;
+class lxc
+{
+public:
+	lxc();
+	~lxc();
+	void readFile(string fileName, string &str);
+	void split(string str, list<string> &list);
+	void run(string command, int i);
+	void run(string command);
+	void installPackage();
+	void create_container(string container, string dist, string relese, string arch);
+	void start_container(string container);
+	void ls_container();
+	void ls_container_list(list <string> &ls);
+	void info_container(string container);
+	void stop_container(string container);
+	void copy_container(string container, string copy_name);
+	void del_container(string container);
+	void freeze_container(string container);
+	void unfreeze_container(string container);
+	void memory_limit(string container, int M);
+};
